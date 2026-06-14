f8f4feaf7d1a4e351afac0901c4e8e8212e971f5
Peter Calapi Madera <pcalapi@hotmail.com>
2026-06-14 11:17:19 -0500

    Rename basura_pruebas to basura_pruebas_git

--- seccion11_archivos/Program.cs
+++ seccion11_archivos/Program.cs
@@ -174,7 +174,7 @@ namespace seccion11_archivos
         
         }
 
-        static void basura_pruebas()
+        static void basura_pruebas_git()
         {
             if (!File.Exists("primer_archivo.txt"))     // busca en la ruta predeterminada del proyecto
             {
