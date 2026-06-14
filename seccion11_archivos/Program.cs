10555c56325313910508591bd51e1fd3baf3db88
Pedro Calapi Madera <palapi@hotmail.com>
2026-06-14 11:15:18 -0500

    no se pudo desde VS subo desde terminal basura cambiado

--- seccion11_archivos/Program.cs
+++ seccion11_archivos/Program.cs
@@ -174,7 +174,7 @@ namespace seccion11_archivos
         
         }
 
-        static void basura()
+        static void basura_pruebas()
         {
             if (!File.Exists("primer_archivo.txt"))     // busca en la ruta predeterminada del proyecto
             {
--- seccion11_archivos/obj/Debug/net10.0-windows/seccion11_archivos.AssemblyInfo.cs
+++ seccion11_archivos/obj/Debug/net10.0-windows/seccion11_archivos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seccion11_archivos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d93c65548bfaf30315c8a445f43891ac4efd50")]
 [assembly: System.Reflection.AssemblyProductAttribute("seccion11_archivos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seccion11_archivos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
